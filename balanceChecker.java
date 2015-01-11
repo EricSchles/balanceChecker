291ed5881a9a2c6efcac273e2368d5f47a9d33d3
EricSchles <ericschles@gmail.com>
2015-01-10 23:36:21 -0500

    balance checker

--- balanceChecker.java
+++ balanceChecker.java
@@ -10,8 +10,27 @@ class Stack<T>{
 		arr.add(input);
 	}
 
+	public <T> void push(T input){
+		arr.add(input);
+	}
+
+	public Object pop(){
+		return arr.remove(arr.size() -1);
+	}
 }
 
 public class balanceChecker{
+	Stack s;
+	String str;
 
+	balanceChecker(String str){
+		s = Stack();
+		for(String line : str){
+			s.push(line);
+		}
+	}
+
+	public Boolean check(String pairs){
+		pairs.split(pairs);
+	}
 }
