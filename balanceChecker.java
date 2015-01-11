737a4dcfed535e3c5f0ffdfa8d05f16a4c653e47
EricSchles <ericschles@gmail.com>
2015-01-10 23:25:08 -0500

    new commit

--- balanceChecker.java
+++ balanceChecker.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 
-class 
+class Stack<T>{
+	private ArrayList arr;
+	public Stack(){
+		arr = new ArrayList();
+	}
+	public <T> Stack(T input){
+		arr = new ArrayList();
+		arr.add(input);
+	}
+
+}
 
 public class balanceChecker{
 
